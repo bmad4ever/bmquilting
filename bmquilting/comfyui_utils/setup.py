6018093ca8a580d5a4fcb90b64f87b6ebafc8269
Bruno Madeira <bmad.works@gmail.com>
2024-09-05 19:43:32 +0100

    maybe this?

--- bmquilting/comfyui_utils/setup.py
+++ bmquilting/comfyui_utils/setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='comfyui-utils',
     version='1.0.0',
-    packages=find_packages(),
+    packages=find_packages(where='bmquilting', include=['bmquilting.comfy_utils', 'bmquilting.comfy_utils.*']),
+    package_dir={'': 'bmquilting'},
     install_requires=[
         'numpy>=1.23.5',
         'joblib~=1.3.2',
