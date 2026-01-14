6aa232f018ec4749f8cfe11c1f99e77633cba1fc
Bruno Madeira <bmad.works@gmail.com>
2026-01-14 23:14:51 +0000

    updated setup.py
    mind to investigate TODO later:
    
    scikit is only being used for block size estimation (bse) which is not a core functionality and was somewhat winged, so having an obligatory dependency here seems unnecessary.
    If I don't get rid of the dependency, adding an extra require for bse funcs also seems like an annoyance for potential users...
    
    TBD

--- setup.py
+++ setup.py
@@ -4,24 +4,23 @@ setup(
     name='bmquilting',
     author="Bruno Madeira",
     author_email="bmad.works@gmail.com",
-    version='1.0.0',
+    version='2.0.0a1',   # TODO remove suffix on release
     packages=find_packages(exclude=[
-        'bmquilting.bmq_comfyui_utils',
-        'bmquilting.bmq_comfyui_utils.*'
-        ]
+        'bmquilting.extras', 
+        'bmquilting.extras.*']
     ),
     install_requires=[
         'numpy>=1.23.5',
         'joblib>=1.3.2',
         'opencv-python>=4.0.1.24',
-        'scikit-learn>=1.3.1',
+        'pyastar2d==1.0.6',
+        'scikit-learn>=1.3.1',  # TODO only used by bse_desc_util.py, consider making it an extra.
     ],
     extras_require={
-        'astar': ['pyastar2d==1.0.6'],
         'fast': ['numba>=0.62.1'],
-        'comfy': [
-            'bmq-comfyui-utils @ git+https://github.com/bmad4ever/bmquilting.git#egg=bmq-comfyui-utils&subdirectory=bmquilting/bmq_comfyui_utils'
-        ]
+        'extras': [
+            'bmq-extras @ git+https://github.com/bmad4ever/bmquilting.git#egg=bmq-extras&subdirectory=bmquilting/extras'
+        ],
     }
 )
 
