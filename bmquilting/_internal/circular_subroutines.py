bmad4ever/bmquilting

commit b4b01f251f09c37caca853424312f3cf2ee1f7c7

refactoring
- create ValidatedTexturesIterator Protocol for TextureInfo and SharedTextureList
- extracted method _get_random_valid_block to common.py
- set_random_patch_at_location at circular_subroutines.py now uses previously defined methods instead of repeating code which is prone to failure due to eventual mismatch
- other minor changes

bmquilting/_internal/circular_subroutines.py
@@ -4,16 +4,16 @@ from dataclasses import dataclass, field
 from collections.abc import Callable
 from functools import lru_cache
 from enum import Enum
+
 import numpy as np
 import pyastar2d
 import cv2
 
 from .common import (
-    avg_squared_diff, adjust_errors_for_pystar2d_inplace,
-    _filter_candidate_patches, _select_a_random_patch, blend_with_mask, update_seams_map_view, TextureList)
+    avg_squared_diff, adjust_errors_for_pystar2d_inplace, _get_random_valid_block, ValidatedTexturesIterator,
+    _filter_candidate_patches, _select_a_random_patch, blend_with_mask, update_seams_map_view )
 from .seams_blur import BlendConfig, gradients_differences_at_the_seam, create_adaptive_blend_mask
 from .common import NumPixels, Percentage, PatchIdx
-from .shmem_utils import SharedTextureList
 
 
 # region ==== CONFIG DATACLASSES ====
@@ -303,7 +303,7 @@ def get_bbox_idx(x: int, y: int, patch_params: CircularPatchParams) -> tuple[sli
 
 
 def process_patch_at_location(image: np.ndarray, filled_mask: np.ndarray, seams_map: np.ndarray,
-                              lookup_textures: TextureList | SharedTextureList,
+                              lookup_textures: ValidatedTexturesIterator,
                               x: int, y: int,
                               config: CircularPatchingConfig,
                               rng: np.random.Generator) -> tuple[PatchIdx, np.ndarray]:
@@ -487,7 +487,7 @@ def _find_min_cut_circ_endpoints(errors: np.ndarray, roi: np.ndarray, heur_overr
 # endregion "Min Cut" Related Functions  ____END
 
 
-def _find_circular_patch(lookup_textures: TextureList | SharedTextureList,
+def _find_circular_patch(lookup_textures: ValidatedTexturesIterator,
                          block: np.ndarray, mask: np.ndarray,
                          params: CircularPatchingConfig, rng: np.random.Generator) -> PatchIdx:
     """
@@ -585,7 +585,7 @@ def _distance_to_points(points_mask: np.ndarray) -> np.ndarray:
 
 
 def set_random_patch_at_location(image: np.ndarray, filled_mask: np.ndarray,
-                                 lookup_textures: TextureList | SharedTextureList,
+                                 lookup_textures: ValidatedTexturesIterator,
                                  x: int, y: int,
                                  config: CircularPatchingConfig,
                                  rng: np.random.Generator) -> tuple[PatchIdx, np.ndarray]:
@@ -595,70 +595,18 @@ def set_random_patch_at_location(image: np.ndarray, filled_mask: np.ndarray,
 
         :return: patch_idxs, mask
     """
-    radius = config.patch_params.radius
-    center = config.patch_params.center
     block_size = config.patch_params.block_size
-    y1, y2, x1, x2 = y - radius, y + radius + 1, x - radius, x + radius + 1
+    mask = _get_circle_mask(config.patch_params)
 
-    # Random patch selection
-    all_valid_counts = []
-    total_valid = 0
-
-    # Identify all valid patches across all textures
-    for idx in range(len(lookup_textures)):
-        texture = lookup_textures[idx]
-        if texture.shape[0] < block_size or texture.shape[1] < block_size:
-            all_valid_counts.append(0)
-            continue
-
-        if lookup_textures.has_mask(idx):
-            mask = lookup_textures.get_mask(idx)
-            count = np.count_nonzero(~mask)
-        else:
-            h, w = texture.shape[:2]
-            count = (h - block_size + 1) * (w - block_size + 1)
-
-        all_valid_counts.append(count)
-        total_valid += count
-
-    if total_valid == 0:
-        raise ValueError("set_random_patch_at_location: No valid patches found in lookup textures.")
-
-    r = int(rng.integers(total_valid))
-    accumulated = 0
-    rnd_text_idx = rand_h = rand_w = 0
-    for idx, count in enumerate(all_valid_counts):
-        if accumulated + count > r:
-            target_index = r - accumulated
-            texture = lookup_textures[idx]
-
-            if lookup_textures.has_mask(idx):
-                mask = lookup_textures.get_mask(idx)
-                y_t, x_t = np.nonzero(~mask)
-                rand_h = int(y_t[target_index])
-                rand_w = int(x_t[target_index])
-            else:
-                w = texture.shape[1]
-                row_len = (w - block_size + 1)
-                rand_h = target_index // row_len
-                rand_w = target_index % row_len
-
-            rnd_text_idx = idx
-            start_block = lookup_textures[rnd_text_idx][rand_h:rand_h + block_size, rand_w:rand_w + block_size]
-            break
-        accumulated += count
-
-    # Create circular mask
-    mask = np.zeros((block_size, block_size), dtype=filled_mask.dtype)
-    cv2.circle(mask, (center, center), radius, (1,), -1)
+    patch_idx, start_block = _get_random_valid_block(block_size, lookup_textures, rng)
 
     # Update image & Filled mask
-    bbox_idx = np.s_[y1:y2, x1:x2]
+    bbox_idx = get_bbox_idx(x, y, config.patch_params)
     np.maximum(mask, filled_mask[bbox_idx], out=filled_mask[bbox_idx])
     np.subtract(1, mask, out=mask)
     blend_with_mask(start_block, image[bbox_idx], mask, out=image[bbox_idx])
 
-    return (rnd_text_idx, rand_h, rand_w), mask
+    return patch_idx, mask
 
 
 def _setup_vignette(roi: np.ndarray, patch_params: CircularPatchParams, dst: np.ndarray = None) -> np.ndarray:

bmquilting/_internal/common.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 from collections.abc import Iterator
+from typing import Protocol
+
 from numpy.random import Generator
 import numpy as np
 import cv2
@@ -24,8 +26,27 @@ type _2D_Slice = tuple[slice, slice]
 FAKE_OUTLIER: int = 4
 """Value used to fill holes in textures."""
 
+class ValidatedTexturesIterator(Protocol):
+    def __iter__(self) -> Iterator[np.ndarray]:
+        """Should iterate the textures."""
+        ...
 
-class TextureList:
+    def __getitem__(self, index: int) -> np.ndarray:
+        """Should return the texture."""
+        ...
+
+    def __len__(self) -> int:
+        ...
+
+    def has_mask(self, idx: int) -> bool:
+        ...
+
+    def get_mask(self, idx:int) -> np.ndarray:
+        ...
+
+
+
+class TextureList(ValidatedTexturesIterator):
     """
     Similar to a list of textures, but makes the necessary adjustments in order to be used with match template
     in the eventual presence of invalid data which is interpreted as holes in the texture.
@@ -129,6 +150,57 @@ def blend_with_mask(bg: np.ndarray, fg: np.ndarray, mask: np.ndarray, out: np.nd
 
 
 # region --- PATCH SELECTION METHODS ---
+
+def _get_random_valid_block(block_size: int, lookup_textures: ValidatedTexturesIterator, rng: Generator)\
+        -> tuple[PatchIdx, np.ndarray]:
+    # Random patch selection
+    all_valid_counts = []
+    total_valid = 0
+
+    # Identify all valid patches across all textures
+    for idx in range(len(lookup_textures)):
+        texture = lookup_textures[idx]
+        if texture.shape[0] < block_size or texture.shape[1] < block_size:
+            all_valid_counts.append(0)
+            continue
+
+        if lookup_textures.has_mask(idx):
+            mask = lookup_textures.get_mask(idx)
+            count = np.count_nonzero(~mask)
+        else:
+            h, w = texture.shape[:2]
+            count = (h - block_size + 1) * (w - block_size + 1)
+
+        all_valid_counts.append(count)
+        total_valid += count
+
+    if total_valid == 0:
+        raise ValueError("set_random_patch_at_location: No valid patches found in lookup textures.")
+
+    r = int(rng.integers(total_valid))
+    accumulated = 0
+    for idx, count in enumerate(all_valid_counts):
+        if accumulated + count > r:
+            target_index = r - accumulated
+            texture = lookup_textures[idx]
+
+            if lookup_textures.has_mask(idx):
+                mask = lookup_textures.get_mask(idx)
+                y_t, x_t = np.nonzero(~mask)
+                rand_h = int(y_t[target_index])
+                rand_w = int(x_t[target_index])
+            else:
+                w = texture.shape[1]
+                row_len = (w - block_size + 1)
+                rand_h = target_index // row_len
+                rand_w = target_index % row_len
+
+            rnd_text_idx = idx
+            start_block = lookup_textures[rnd_text_idx][rand_h:rand_h + block_size, rand_w:rand_w + block_size]
+            return (rnd_text_idx, rand_h, rand_w), start_block
+        accumulated += count
+    raise RuntimeError("Reached unreachable code: target not found in items.")
+
 
 def _filter_candidate_patches(err_mats: list[np.ndarray | None],
                               global_min_error: float , tolerance: float) -> list[tuple[int, int, int]]:

bmquilting/_internal/shmem_utils.py
@@ -9,7 +9,7 @@ import time
 import os
 import gc
 
-from .common import process_invalid_data
+from .common import process_invalid_data, ValidatedTexturesIterator
 
 import logging
 
@@ -147,7 +147,7 @@ def get_individual_mask(metadata: TextureMetadata, index: int) -> np.ndarray | N
 
 # --- 3. SEQUENCE PROTOCOL WRAPPER (Public Interface) ---
 
-class SharedTextureList:
+class SharedTextureList(ValidatedTexturesIterator):
     """
     The main class for creating, managing, and accessing shared, variable-sized
     NumPy arrays via a single memory-mapped file.

bmquilting/circular.py
@@ -175,17 +175,17 @@ def _scale_proxy_patch_list(proxy_data: list[ProxyPatch] | ListProxy[ProxyPatch]
              for idx, mask, tx, ty in proxy_data]
 
 
-def _get_patch(textures: list[np.ndarray] | SharedTextureList, idx: PatchIdx, block_size: NumPixels) -> np.ndarray:
-    texture_index = idx[0]
-    py, px = idx[1], idx[2]
-    block_coords = np.s_[py:py + block_size, px:px + block_size]
-    return textures[texture_index][block_coords]
-
-
 def _paste_fromproxy(target: np.ndarray,
                      proxy_data_item: ProxyPatch, pp: CircularPatchParams,
                      source_textures: SharedTextureList | list[np.ndarray] ) -> None:
     patch_idx, mask, tx, ty = proxy_data_item   # tx, ty -> the patch center coordinate on the target texture
+
+    def _get_patch(textures, idx, block_size) -> np.ndarray:
+        texture_index = idx[0]
+        py, px = idx[1], idx[2]
+        block_coords = np.s_[py:py + block_size, px:px + block_size]
+        return textures[texture_index][block_coords]
+
     patch = _get_patch(source_textures, patch_idx, pp.block_size)
     target_idx = get_bbox_idx(tx, ty, pp)
     region = target[target_idx]

commit 9f4c04c981bb0eeb3aac9dec971cbdc7c0eb061c

fix bug introduced in b4b01f251f09c37caca853424312f3cf2ee1f7c7
the mask is cached but was not being copied, consequently inverted when setting the 1st random patch.

bmquilting/_internal/circular_subroutines.py
@@ -596,7 +596,7 @@ def set_random_patch_at_location(image: np.ndarray, filled_mask: np.ndarray,
         :return: patch_idxs, mask
     """
     block_size = config.patch_params.block_size
-    mask = _get_circle_mask(config.patch_params)
+    mask = _get_circle_mask(config.patch_params).copy()
 
     patch_idx, start_block = _get_random_valid_block(block_size, lookup_textures, rng)
 