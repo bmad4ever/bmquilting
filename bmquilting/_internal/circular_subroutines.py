fix similar ooopsies & removed devnote regarding distanceTransform precision

bmquilting/_internal/circular_subroutines.py
@@ -575,7 +575,7 @@ def _distance_to_points(points_mask: np.ndarray) -> np.ndarray:
         the closer to the points the closer to one.
     """
     np.subtract(1, points_mask, out=points_mask)
-    distance_map = cv2.distanceTransform(points_mask, cv2.DIST_L2, 5, dst=points_mask, dstType=cv2.CV_32F)
+    distance_map = cv2.distanceTransform(points_mask, cv2.DIST_L2, cv2.DIST_MASK_3, dst=points_mask, dstType=cv2.CV_32F)
     ksize = min(min(distance_map.shape) // 4, 15) | 1
     dst_norm = cv2.GaussianBlur(distance_map, (ksize, ksize), sigmaX=0, sigmaY=0, dst=distance_map)
     cv2.normalize(dst_norm, dst_norm, 0, 1, cv2.NORM_MINMAX, dtype=cv2.CV_32F)

bmquilting/_internal/seams_blur.py
@@ -343,13 +343,8 @@ def create_adaptive_blend_mask(tdiff_map: np.ndarray, mc_mask_overlap: np.ndarra
         cv2.GaussianBlur(blend_radii, (0, 0), sigmaX=sigma, sigmaY=sigma, dst=blend_radii)
 
     # Calculate signed distance from transition line
-    # dev note: compared 3 vs cv2.DIST_MASK_PRECISE
-    #   both performed well in terms of speed and precision so far
-    #   doesn't seem like something that needs to be further tested
-    #   as any minor details will be blured out.
-    #   I will choose simples-fastest for now
-    dist_from_source = cv2.distanceTransform((mc_mask_overlap > 0).astype(np.uint8), cv2.DIST_L2, 3)
-    dist_from_patch = cv2.distanceTransform((mc_mask_overlap <= 0).astype(np.uint8), cv2.DIST_L2, 3)
+    dist_from_source = cv2.distanceTransform((mc_mask_overlap > 0).astype(np.uint8), cv2.DIST_L2, cv2.DIST_MASK_3)
+    dist_from_patch = cv2.distanceTransform((mc_mask_overlap <= 0).astype(np.uint8), cv2.DIST_L2, cv2.DIST_MASK_3)
     dist_from_patch -= dist_from_source  # compute in place
     signed_distance = dist_from_patch.astype(np.float32)
     cv2.GaussianBlur(signed_distance, (0, 0), sigmaX=1.0, sigmaY=1.0, dst=signed_distance)